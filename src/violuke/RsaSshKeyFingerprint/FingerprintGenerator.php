a5820b520b4a1858b451d68d943e4420ffaff6a4
Erik Peterson <erik.peterson@acquia.com>
2017-05-22 16:36:49 +0000

    Adding SHA256 Hash Algorithm option to the fingerprint generation.

--- src/violuke/RsaSshKeyFingerprint/FingerprintGenerator.php
+++ src/violuke/RsaSshKeyFingerprint/FingerprintGenerator.php
@@ -3,13 +3,23 @@
 namespace violuke\RsaSshKeyFingerprint;
 
 class FingerprintGenerator {
-    public static function getFingerprint($sshPublicKey){
+    public static function getFingerprint($sshPublicKey, $hashAlgorithm = 'md5'){
         if (substr($sshPublicKey, 0, 8) != 'ssh-rsa '){
             throw new InvalidInputException('All keys must start with "ssh-rsa ".');
         }
 
+
         $content = explode(' ', $sshPublicKey, 3);
-        $fingerprint = join(':', str_split(md5(base64_decode($content[1])), 2));
+        switch ($hashAlgorithm) {
+            case 'md5':
+                $fingerprint = join(':', str_split(md5(base64_decode($content[1])), 2));
+                break;
+            case 'sha256':
+                $fingerprint = base64_encode(hash('sha256', base64_decode($content[1]), true));
+                break;
+            default:
+                throw new InvalidInputException('Valid algorithms are md5 and sha256');
+        }
 
         return $fingerprint;
     }
